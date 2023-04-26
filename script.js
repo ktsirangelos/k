6d51566e288e1ff2552cc8b781c3d2ebd910f7a1
Konstantinos Tsirangelos <ktsirangelos@proton.me>
2023-04-26 15:28:41 +0200

    new outline

--- script.js
+++ script.js
@@ -1,7 +1,1 @@
 "use strict";
-
-const changeText = (event) => {
-  const link = document.getElementById("thisWebsiteProd");
-  link.text = link.text === "Go to Site" ? "already here!" : "Go to Site";
-  event.preventDefault();
-};
