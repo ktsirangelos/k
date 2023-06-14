776cb5982b6ae7e5b0ea5ed99559efa8de336204
Konstantinos Tsirangelos <ktsirangelos@proton.me>
2023-06-14 15:18:14 +0200

    responsive mobile & tablet portrait

---
+++ js/script.js
@@ -0,0 +1,8 @@
+"use strict";
+
+function changeText() {
+  document.getElementById("this").textContent =
+    document.getElementById("this").textContent === "this website"
+      ? "already here -.-"
+      : "this website";
+}
