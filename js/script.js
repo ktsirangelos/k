ae3fab13d007c02f5405a1a895e1de6aefa01f4f
Konstantinos Tsirangelos <ktsirangelos@proton.me>
2023-06-15 11:54:46 +0200

    arrows

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@
 
 function changeText() {
   document.getElementById("this").textContent =
-    document.getElementById("this").textContent === "this website"
-      ? "already here"
-      : "this website";
+    document.getElementById("this").textContent === "this website →"
+      ? "already here ↩"
+      : "this website →";
 }
